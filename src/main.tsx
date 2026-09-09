ab3b968fd01f530ea8bab2f571350e165f7e5957
Glein <edquilaggleinardian@gmail.com>
2026-09-09 21:33:12 +0800

    Provide marketplace context at app root

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { MarketplaceProvider } from './context/MarketplaceContext'
 import './index.css'
 
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <MarketplaceProvider>
+        <App />
+      </MarketplaceProvider>
     </BrowserRouter>
   </StrictMode>,
 )
